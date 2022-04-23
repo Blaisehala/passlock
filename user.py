1f43fda16f006083f4ed185ba19eb19f65540444
Blaise Hala <blaisehala@gmail.com>
2022-04-24 02:32:16 +0300

    creates class User  to generate new instance of user

--- user.py
+++ user.py
@@ -1,4 +1,15 @@
 
 
 class User:
-  def __init__(self,)
+  '''
+  This class represents/generates the new instance of user
+  '''
+  user_list = []
+
+  def __init__(self,usernamme,password):
+    '''
+    The __init__ method helps to define properties for our objects.The __init__ function is called every time an object is created from a class. 
+    '''
+    
+    self.usernamme = usernamme
+    self.password = password
