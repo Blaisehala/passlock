3d1fe4deef6960dd56b97dd06d02716660ad93df
Blaise Hala <blaisehala@gmail.com>
2022-04-25 02:58:45 +0300

    creation of function to save new user

--- run.py
+++ run.py
@@ -3,10 +3,20 @@
 from user import User,Credentials
 
 
-""" Function create_user creates a new user"""
-def create_user(username, password):
-    new_user = User(username, password)
+
+def make_user(username, password):
+    """ Function create_user takes in two arguments and creates a new user"""
+    new_user = User(username, password) #Here we create a new object  new_user and return it
     return new_user
 
 
+def s_user(user):
+    ''' Function to save user'''
+    user.save_user() #The Function takes in the user object and then calls the save_user  method to save the user.
 
+
+
+    
+
+
+     
