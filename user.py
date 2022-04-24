485e1b025d5b5e11496f8d78a09b06b2839120e0
Blaise Hala <blaisehala@gmail.com>
2022-04-24 04:09:33 +0300

    adds instances of saving and deleting users to the User class

--- user.py
+++ user.py
@@ -13,3 +13,15 @@ class User:
     
     self.usernamme = usernamme
     self.password = password
+  
+  def save_user(self):
+    '''
+    Save_user method saves the user object into the user list.
+    '''
+    User.user_list.append(self)
+
+  def delete_user(self):
+    '''
+    delete_user method deletes the user object from the user list.
+    '''
+    User.user_list.remove(self)
