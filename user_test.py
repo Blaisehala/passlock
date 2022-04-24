6fe44859931d0487a5c2ee6110ff121e1b4323ca
Blaise Hala <blaisehala@gmail.com>
2022-04-24 13:35:02 +0300

    test:test for initialization of class credentials

--- user_test.py
+++ user_test.py
@@ -1,5 +1,6 @@
 import unittest #importing unittest module
-from user import User  #importing user
+from user import User  #importing user2
+from user import Credentials
 
 class TestUser(unittest.TestCase):
   '''Test class for testing cases for the user class behavior
@@ -32,6 +33,24 @@ class TestUser(unittest.TestCase):
 
     self.new_user.delete_user()
     self.assertEqual(self.new_user) #CHECK CHECK
+
+
+#Setting test Case for testing the class credentials
+
+class TestCredentials(unittest.TestCase):
+  '''Test credentials for testing cases for the credentials class behaviour'''
+
+  def setUp(self):#Set up  the method to run each test case
+    self.new_credentials=Credentials("twitter",'Blaise','!@#$%^&*')
+
+#Fourth test Case
+  def test_init(self):
+    '''method tests for proper initialization'''
+    self.assertEqual(self.new_credentials.account,'twitter')
+    self.assertEqual(self.new_credentials.username,'Blaise')
+    self.assertEqual(self.new_credentials.password,'!@#$%^&*')
+
+
 
 
 if __name__ == '__main__':
