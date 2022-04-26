b0e24d0153c69d1f63c76275f03777d6e53bf244
Blaise Hala <blaisehala@gmail.com>
2022-04-26 05:33:55 +0300

    rf:refactoring and clean up of code plus addition of logic

--- run.py
+++ run.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3.8
-
-
-from user import User,Credentials
+from user import User, Credentials
 
 
 
 def make_user(username, password):
     """ Function create_user takes in two arguments and creates a new user"""
-    new_user = User(username, password) #Here we create a new object  new_user and return it
+    new_user = User(username, password)  # Here we create a new object  new_user and return it
     return new_user
 
 
 def s_user(user):
     ''' Function to save user'''
-    user.save_user() #The Function takes in the user object and then calls the save_user  method to save the user.
+    User.save_user(user) # The Function takes in the user object and then calls the save_user  method to save the user.
 
 
 def del_user(user):
     ''' Function to delete user'''
-    user.delete_user()
+    User.delete_user(user)
 
 
-
-def make_credentials(account,username,password):
+def make_credentials(account, username, password):
     ''' Function to create new account credentials'''
-    new_credentials = Credentials(account,username,password)
+    new_credentials = Credentials(account, username, password)
     return new_credentials
 
 
@@ -33,84 +30,160 @@ def s_credentials(user_credentials):
     user_credentials.save_credentials()
 
 
-
 def del_credentials(user_credentials):
     ''' Function to  delete user credentials'''
     user_credentials.delete_credentials()
-     
-def login(name,password):
-    return Credentials.verify_user(name,password)
+
+
+def login(name, password):
+    return Credentials.verify_user(name, password)
+
+
+def generatePassword(ln):
+    return Credentials.generatePassword(ln)
 
 
 def main():
-  print('Hola! Welcome to WatchWord.')
-    
-  while True:
-      '''
-      cu-create-user
-      lg-login
-      ex-exit
-      (shortcodes to be used while navigating the  app)  
-      '''
-      print("Use the following Shortcodes for your navigation🧭:\ncu -Create User\nlg -login\nex-exit")
-      shortcodes = input().lower().strip()
-      if shortcodes == "cu":
-          print("Enter user details")
-          print("-------------------")
-          username = input('USERNAME\n')
+    print('Hola! Welcome to WatchWord.')
 
-          while True:
-              print("Enter tp - to type your password\n gp - to generate password")
-              password_choice = input('Choice').lower().strip()
+    while True:
+        '''
+        cu-create-user
+        lg-login
+        ex-exit
+        (shortcodes to be used while navigating the  app)  
+        '''
+        print("Use the following Shortcodes for your navigation🧭:\ncu -Create User\nlg -login\nex-exit")
+        shortcodes = input().lower().strip()
+        if shortcodes == "cu":
+            print("Enter user details")
+            print("-------------------")
+            username = input('USERNAME\n')
 
-              if password_choice == 'tp':
-                  password = input('Password\n')
-                  break
-              elif password_choice == 'gp':
-                  password = Credentials.generatePassword
-                  break
-              else:
-                  print("Invalid password***.Use the shorcodes to generate password ")
+            while True:
+                print("Enter tp - to type your password\n gp - to generate password")
+                password_choice = input('Choice').lower().strip()
 
-          newuser = make_user(username, password)
-        
-      elif shortcodes== 'li':
-            print ('Enter details to login')
-            username= input('username')
-            password= input('password')
+                if password_choice == 'tp':
+                    password = input('Password\n')
+                    break
+                    
+                elif password_choice == 'gp':
+                    password = Credentials.generatePassword(12)
+                    print(password)
+                    break
+                else:
+                    print("Invalid password***.Use the shorcodes to generate password ")
 
-          
-
-
-
+            s_user(make_user(username, password))
+            print(f"✅Created user{username} {password}")
             
 
+            newuser = make_user(username, password)
+
+
+        elif shortcodes=="ex":
+            print('✘ exited')
+            break
+
         
 
 
+        elif shortcodes == 'lg':
+            print('☎ Enter details to login')
+            username = input('username')
+            password = input('password')
+
+
+
+            user = login(username, password)
+            print(f' ⛩ welcome to Watchword {username}')
+
+            # break
+          
+
+            # if user == username:
+                # print(f'welcome{username}') 
+
+            # elif user ==password:
+            #       print(f'welcome{username}')
+                
+                
+                 # f string to make string interpolation simpler
+
+            while True:
+                print('*' *60)
+                print(
+                        'Use the following shortcodes for your way around:\ncc -Create new credentials \ndc -Display credentials\nfc-Find credentials\ndel-Delete credentials\next -exit app\n')
+
+                shortcodes == input("Enter Option:").lower().strip()
+               
+                if shortcodes == 'ext':
+                    print('Bye Bye')
+                    break
+
+                elif shortcodes=='cc':
+                     print('Save credentials.')
+                     account = input('Enter account name:').strip()
+                     username= input('Account username:').strip()
+                     while True:
+                         print('Enter tp- to type password\n gp- to generate new password')
+                         password_choice = input('Choice:').strip()
+                         if password_choice == 'tp':
+                             password= input('password')
+
+                             break
+                        
+                         elif password_choice == 'gp':
+                             password= generatePassword(8)
+                             break
+
+                         else:
+                             print('Invalid password***.Use the shorcodes to generate password')
+
+                     make_credentials(s_credentials(account, username, password))
+
+                     print(f'Your{account} has been created.\n Your Username is {username} and Password is {password}')
+
+                elif shortcodes== 'fc':
+                    print("Enter your account name")
+                    find_credentials= input('Account Name').strip()
+
+                    
 
 
 
 
 
-if __name__=='__main__':
-  main()
+                        
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
 
 # print('Blaise')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- user.py
+++ user.py
@@ -1,79 +1,71 @@
-
-
 from random import randint
 
 
 class User:
-  '''
-  This class represents/generates the new instance of user
-  '''
-
-  def __init__(self,username,password):
     '''
-    The __init__ method helps to define properties for our objects.The __init__ function is called every time an object is created from a class. 
+    This class represents/generates the new instance of user
     '''
 
-    self.username = username
-    self.password = password
+    def __init__(self, username, password):
+        '''
+        The __init__ method helps to define properties for our objects.The __init__ function is called every time an object is created from a class.
+        '''
 
-  user_list = []
+        self.username = username
+        self.password = password
 
+    user_list = []
 
-  def save_user(self):
-    '''
-    Save_user method saves the user object into the user list.
-    '''
-    User.user_list.append(self)
+    def save_user(self):
+        '''
+        Save_user method saves the user object into the user list.
+        '''
+        User.user_list.append(self)
 
-  def delete_user(self):
-    '''
-    delete_user method deletes the user object from the user list.
-    '''
-    User.user_list.remove(self)
-
+    def delete_user(self):
+        '''
+        delete_user method deletes the user object from the user list.
+        '''
+        User.user_list.remove(self)
 
 
 class Credentials:
-  def __init__(self,account,username, password):
-    self.account = account
-    self.username = username
-    self.password = password
+    def __init__(self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
 
-  credentials_list = []
-  def save_credentials(self):
-    '''Saves the  credentials in the  credentials list.'''
-    Credentials.credentials_list.append(self)
+    credentials_list = []
 
-  def delete_credentials(self):
-    Credentials.credentials_list.remove(self)
+    def save_credentials(self):
+        '''Saves the  credentials in the  credentials list.'''
+        Credentials.credentials_list.append(self)
 
+    def delete_credentials(self):
+        Credentials.credentials_list.remove(self)
 
-  @classmethod
-  def display_credentials(cls):
-    return cls.credentials_list
+    @classmethod
+    def display_credentials(cls):
+        return cls.credentials_list
 
-  @classmethod
-  def search_credentials(cls, account):
-    for credential in cls.credentials_list:
-      if credential.account == account:
-        return credential
+    @classmethod
+    def search_credentials(cls, account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
 
-@classmethod
-def verify_user(cls, username,password):
-  active_user =""
-  for user in User.user_list:
-      if (user.username == username and user.password == password):
-         active_user = user,username
+    @classmethod
+    def verify_user(cls, username, password):
+        active_user = ""
+        for user in User.user_list:
+            if (user.username == username and user.password == password):
+                active_user = user, username
 
-  return active_user
+        return active_user
 
-
-
-
-
-  def generatePassword(ln):
-      characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'
-      password= ''
-      for i in range(ln):
-          password += characters[randint(0,len(characters)-1)]
-      return password
+    def generatePassword(ln):
+        characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'
+        password = ''
+        for i in range(ln):
+            password += characters[randint(0, len(characters) - 1)]
+        return password
